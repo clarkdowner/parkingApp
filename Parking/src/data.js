12d9557f7e35e86875ad254f16e2a49e8688a077
clarkdowner <cddowner@gmail.com>
2017-03-04 15:14:34 -0800

    Initialize react-native app with temp data

---
+++ Parking/src/data.js
@@ -0,0 +1,43 @@
+export const street_table = [
+  {
+    id: 0, 
+    street_name: 'Divisadero',
+    weekly_freq: 'EVERY',
+    daily_freq: [0,1,0,1,0,0,1],
+    time_of_day: 6,    
+  },
+  {
+    id: 1, 
+    street_name: 'Fell',
+    weekly_freq: '1&3',
+    daily_freq: [0,0,0,0,0,0,0],
+    time_of_day: 4,    
+  },
+  {
+    id: 2, 
+    street_name: 'Oak',
+    weekly_freq: '2&4',
+    daily_freq: [0,0,0,0,0,0,0],
+    time_of_day: 10,    
+  },
+  {
+    id: 3, 
+    street_name: 'Baker',
+    weekly_freq: 'EVERY',
+    daily_freq: [0,0,0,0,0,1,0],
+    time_of_day: 9,    
+  },
+  {
+    id: 4, 
+    street_name: 'Grove',
+    weekly_freq: 'EVERY',
+    daily_freq: [0,1,0,0,0,0,0],
+    time_of_day: 12,    
+  },
+];
+
+export const current_parked = {
+  on: street_table[3],
+  at: street_table[4],
+  expire_time: 'Date'
+};
